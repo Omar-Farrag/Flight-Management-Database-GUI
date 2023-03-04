f42a3409aac6095bc506aa78f4b49378aa33a9bf
Omar Khaled Farrag <omar.farrag716@gmail.com>
2023-03-04 23:18:44 +0400

    Added the DatabaseManagement package to commit

---
+++ DatabaseManagement/DatabaseOperations.java
@@ -0,0 +1,19 @@
+package DatabaseManagement;
+
+public interface DatabaseOperations {
+    // Just a simple iterface for our database manager class
+
+    public void establishConnection();
+
+    // Inserts an entry into the database
+    public void insert();
+
+    // Deletes a set of entries from the database
+    public void delete();
+
+    // Modifies a set of entries in the database
+    public void modify();
+
+    // retrieves data from the database
+    public void retrieve();
+}
